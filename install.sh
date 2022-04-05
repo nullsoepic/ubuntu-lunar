e09f4867d4e070310a04a0dde7f9ca339b65909b
Null <83729285+nullsoepic@users.noreply.github.com>
2022-04-05 15:00:52 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,6 @@
-sudo apt install openjdk-8-jre-headless && curl https://launcherupdates.lunarclientcdn.com/Lunar%20Client-2.10.0.AppImage > LunarClient.AppImage && $ chmod u+x LunarClient.AppImage && echo LunarClient successfuly installed!
+#!/bin/bash
+sudo apt install openjdk-8-jre-headless
+curl https://launcherupdates.lunarclientcdn.com/Lunar%20Client-2.10.0.AppImage > LunarClient.AppImage
+chmod u+x LunarClient.AppImage
+./LunarClient.AppImage
+echo Install successful!
